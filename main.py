b8731f40478cb51c98729c8fd0ff603d0cc0a727
Joe <87231971+boejello@users.noreply.github.com>
2021-08-23 09:29:42 -0500

    Add files via upload
    scrapes multiple pages functionality
    dynamic page numbering
    url template improvements

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import Select
 
 position = 'construction engineer'
 location = 'chicago il'
+pageMax = 5
 
 
 def get_url(position, location, page): # Generate url from position and location and start page
@@ -16,33 +17,31 @@ def get_url(position, location, page): # Generate url from position and location
     url = template.format(position, location, page)
     return url
 
-
-for page in range(0,5): # Get list of urls
+urls = []
+for page in range(0,pageMax): # Get list of urls
     urls = get_url(position, location, page)
     response = requests.get(urls)
-    print(urls)
-
-
-for url in urls:
     soup = BeautifulSoup(response.content, 'html.parser')
+    print(urls)
 
 pageIndex = 1
 
-for job in soup.select('.result'): # Get job info from url
-    jobName = job.select_one('.jobTitle').get_text(' ')
-    company = job.select_one('.companyName').get_text(' ')
-    location = job.select_one('.companyLocation').get_text(' ')
-    jobLink = f'https://indeed.com/viewjob?jk={job["data-jk"]}'
-    print("Job Listing #", pageIndex)
-    print(jobName,"-", location)
-    print(company, jobLink)
-    try:
-        salary = job.select_one('.salary-snippet-container').get_text(' ')
-        print(salary)
-    except AttributeError:
-        pass  # no salary listed error handling
-    print("-----")
-    pageIndex +=1
+for urls in range(0,pageMax): # Get job info from multiple urls
+    for job in soup.select('.result'): # Get job info from single url
+        jobName = job.select_one('.jobTitle').get_text(' ')
+        company = job.select_one('.companyName').get_text(' ')
+        location = job.select_one('.companyLocation').get_text(' ')
+        jobLink = f'https://indeed.com/viewjob?jk={job["data-jk"]}'
+        print("Job Listing #", pageIndex)
+        print(jobName,"-", location)
+        print(company, jobLink)
+        try:
+            salary = job.select_one('.salary-snippet-container').get_text(' ')
+            print(salary)
+        except AttributeError:
+            pass  # no salary listed error handling
+        print("-----")
+        pageIndex +=1
 
 
 
