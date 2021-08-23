e13d226faba502bf70cc14ff2948e508301954ee
Joe <87231971+boejello@users.noreply.github.com>
2021-08-23 17:16:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
-from selenium.webdriver.support.ui import Select
-from selenium import webdriver
-
 
 position = 'construction engineer'
 location = 'chicago il'
-pageMax = 3
+pageMax = 5
 
 def get_url(position, location, page): # Generate url from position and location and start page
     template = 'https://www.indeed.com/jobs?q={}&l={}&radius=10&sort=date&start={}0'
@@ -26,51 +22,28 @@ for page in range(0,pageMax): # Get list of urls
         print("1st Page:", urls)
     else:
         print("Page", page+1, ":", urls )
-print("-----")
+    easyApplyCount = len(soup.find_all("span", class_='ialbl iaTextBlack'))
+    print(f"There are {easyApplyCount} Easy Apply links in this page \n-----")
 
 pageIndex = 1
-
-def indeedApplyTag():
-    if soup.find("span", {"class": "Apply Now"}):
-        print("Apply Now Tag Found!1")
-    if soup.findAll(text="Apply Now"):
-        print("Apply Now Tag Found!2")
-    elif soup.find("td", class_='shelfItem indeedApply'):
-         print("Apply Now Tag Found!3")
-    elif soup.find_all("span", {"class": "Apply Now"}):
-        print("Apply Now Tag Found!4")
-    elif soup.find("div", {"class": "jobsearch-IndeedApplyButton"}):
-        print("Apply Now Tag Found!5")
-    elif soup.find_all("div", {"class": "jobsearch-IndeedApplyButton"}):
-        print("Apply Now Tag Found!6")
-    elif soup.find("div", {"id": "indeedApplyButtonContainer"}):
-        print("Apply Now Tag Found!7")
-    elif soup.find_all("div", {"id": "indeedApplyButtonContainer"}):
-        print("Apply Now Tag Found!8")
-    elif soup.find(itemprop="indeedApplyButtonContainer"):
-        print("Apply Now Tag Found!81")
-    elif job.select_one('.jobsearch-IndeedApplyButton-contentWrapper'):
-        print("Apply Now Tag Found!9")
-    else:
-        print("Indeed Apply Tag Not Found1")
-
+easyApplyIndex = 1
 
 for urls in range(0,pageMax): # Get job info from multiple urls
-    for job in soup.select('.result'): # Get job info from single url
-        jobName = job.select_one('.jobTitle').get_text(' ')
-        company = job.select_one('.companyName').get_text(' ')
-        location = job.select_one('.companyLocation').get_text(' ')
-        jobLink = f'https://indeed.com/viewjob?jk={job["data-jk"]}'
-#if soup.find("span", class_='ialbl iaTextBlack').text == "Easily Apply":
-        print("Job Listing #", pageIndex)
+    for listing in soup.select('.result'): # Get job info from single url
+        jobName = listing.select_one('.jobTitle').get_text(' ')
+        company = listing.select_one('.companyName').get_text(' ')
+        location = listing.select_one('.companyLocation').get_text(' ')
+        print("#", pageIndex)
+        try:
+            easyApply = listing.select_one('.ialbl.iaTextBlack').get_text(' ')
+            print(f'{easyApply} #{easyApplyIndex}')
+            easyApplyIndex +=1
+        except:
+            AttributeError
+        jobLink = f'https://indeed.com/viewjob?jk={listing["data-jk"]}'
         print(jobName,"-", location)
         print(company, jobLink)
-        try:
-            salary = job.select_one('.salary-snippet-container').get_text(' ')
-            print(salary)
-        except AttributeError:
-            pass  # no salary listed error handling
         print("-----")
         pageIndex +=1
-        pass
+
 
