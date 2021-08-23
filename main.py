33aecaac628343311f92c4990d79c458e7e71eda
Joe <87231971+boejello@users.noreply.github.com>
2021-08-23 08:53:06 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,27 +7,24 @@ from selenium.webdriver.support.ui import Select
 position = 'construction engineer'
 location = 'chicago il'
 
-pageList = []
-for n in range(0, 5):
-    pageList.append(n)
-
-pages = pageList
 
 def get_url(position, location, page): # Generate url from position and location and start page
-
-    template = 'https://www.indeed.com/jobs?q={}&l={}&start={}'
+    template = 'https://www.indeed.com/jobs?q={}&l={}&radius=10&sort=date&start={}'
     position = position.replace(' ', '+')
     location = location.replace(' ', '+')
     page = str(page)
     url = template.format(position, location, page)
     return url
 
-for page in pages: # Get list of urls
-    url = get_url(position, location, page)
-    response = requests.get(url)
-    print(f'{url} \n-----')
 
-soup = BeautifulSoup(response.content, 'html.parser')
+for page in range(0,5): # Get list of urls
+    urls = get_url(position, location, page)
+    response = requests.get(urls)
+    print(urls)
+
+
+for url in urls:
+    soup = BeautifulSoup(response.content, 'html.parser')
 
 pageIndex = 1
 
@@ -35,9 +32,8 @@ for job in soup.select('.result'): # Get job info from url
     jobName = job.select_one('.jobTitle').get_text(' ')
     company = job.select_one('.companyName').get_text(' ')
     location = job.select_one('.companyLocation').get_text(' ')
-
     jobLink = f'https://indeed.com/viewjob?jk={job["data-jk"]}'
-    print("Job #", pageIndex)
+    print("Job Listing #", pageIndex)
     print(jobName,"-", location)
     print(company, jobLink)
     try:
@@ -47,4 +43,6 @@ for job in soup.select('.result'): # Get job info from url
         pass  # no salary listed error handling
     print("-----")
     pageIndex +=1
+
+
 
