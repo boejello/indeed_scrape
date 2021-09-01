8bbec9c5a437ed8d30bf9fe2162aee7af8307fa6
Joe <87231971+boejello@users.noreply.github.com>
2021-09-01 15:47:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
 
 position = 'construction engineer'
 location = 'chicago il'
-pageMax = 5
+pageMax = 3
 
-def get_url(position, location, page): # Generate url from position and location and start page
+def get_url(position, location, page): # Generate single url from position and location and start page
     template = 'https://www.indeed.com/jobs?q={}&l={}&radius=10&sort=date&start={}0'
     position = position.replace(' ', '&20')
     location = location.replace(' ', '&20')
@@ -13,18 +15,17 @@ def get_url(position, location, page): # Generate url from position and location
     url = template.format(position, location, page)
     return url
 
-urls = []
+urls = [] # Generate list of urls from position and location and start page
 print(f' Scraping {pageMax} urls for search term: {position} in {location}')
 for page in range(0,pageMax): # Get list of urls
     url = get_url(position, location, page)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    urls.append(url)
     if page == 0:
         print("1st Page:", url)
     else:
         print("Page", page+1, ":", url )
-print("-----")
-
 
 def get_all_jobs():
     print("get_all_jobs()")
@@ -42,12 +43,12 @@ def get_all_jobs():
                 easyApplyIndex +=1
             except:
                 AttributeError
+            jobIndex += 1
             jobLink = f'https://indeed.com/viewjob?jk={listing["data-jk"]}'
             print(jobName,"-", location)
             print(company, jobLink)
             print("-----")
-            jobIndex +=1
-
+    jobIndex +=1
 
 def get_easy_apply_links():
     print("get_easy_apply_links()")
@@ -66,10 +67,10 @@ def get_easy_apply_links():
                 AttributeError
             jobIndex +=1
 
-
 def get_all_jobs_list():
     print("get_all_jobs_list()")
     jobIndex = 1
+    list = []
     for urls in range(0,pageMax): # Get job info from multiple urls
         for listing in soup.select('.result'): # Get job info from single url
             jobName = listing.select_one('.jobTitle').get_text(' ')
@@ -84,5 +85,11 @@ def get_all_jobs_list():
                 print(f'#{jobIndex} - {jobLink} - {jobName} - {company}')
             jobIndex += 1
 
-get_all_jobs()
 
+def csvExport(): # Not working right!
+    df = pd.DataFrame(get_all_jobs_list(), columns=["job index", "joblink", "jobname", "company"])
+    df.to_csv('list.csv', index=False)
+
+get_all_jobs_list()
+get_all_jobs()
+get_easy_apply_links()
