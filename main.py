bc31b605614192f572abf2c6fff4c1461d284587
Joe <87231971+boejello@users.noreply.github.com>
2021-09-03 20:16:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
 
 position = 'construction engineer'
-location = 'chicago il'
-pageMax = 3
+location = 'miami fl'
+pageMax = 5
+
 
 def get_url(position, location, page): # Generate single url from position and location and start page
     template = 'https://www.indeed.com/jobs?q={}&l={}&radius=10&sort=date&start={}0'
@@ -15,81 +18,47 @@ def get_url(position, location, page): # Generate single url from position and l
     url = template.format(position, location, page)
     return url
 
-urls = [] # Generate list of urls from position and location and start page
-print(f' Scraping {pageMax} urls for search term: {position} in {location}')
-for page in range(0,pageMax): # Get list of urls
-    url = get_url(position, location, page)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    urls.append(url)
-    if page == 0:
-        print("1st Page:", url)
-    else:
-        print("Page", page+1, ":", url )
-
-def get_all_jobs():
-    print("get_all_jobs()")
-    jobIndex = 1
-    easyApplyIndex = 1
-    for urls in range(0,pageMax): # Get job info from multiple urls
-        for listing in soup.select('.result'): # Get job info from single url
-            jobName = listing.select_one('.jobTitle').get_text(' ')
-            company = listing.select_one('.companyName').get_text(' ')
-            location = listing.select_one('.companyLocation').get_text(' ')
-            print("Job Listing #",jobIndex)
-            try:
-                easyApply = listing.select_one('.ialbl.iaTextBlack').get_text(' ')
-                print(f'{easyApply} # {easyApplyIndex}')
-                easyApplyIndex +=1
-            except:
-                AttributeError
-            jobIndex += 1
-            jobLink = f'https://indeed.com/viewjob?jk={listing["data-jk"]}'
-            print(jobName,"-", location)
-            print(company, jobLink)
-            print("-----")
-    jobIndex +=1
-
 def get_easy_apply_links():
-    print("get_easy_apply_links()")
-    jobIndex = 1
-    easyApplyIndex = 1
-    for urls in range(0,pageMax): # Get job info from +  multiple urls
+    urls = []  # Generate list of urls from position and location and start page
+    print(f' Scraping {pageMax} urls for search term: {position} in {location}')
+    for page in range(0, pageMax):  # Get list of urls
+        url = get_url(position, location, page)
+        response = requests.get(url, headers={'Cache-Control': 'no-cache'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        urls.append(url)
+        if page == 0:
+            print("List of Easy Apply Jobs from Page 1:", url)
+        else:
+            print("List of Easy Apply Jobs from Page", page + 1, ":", url)
+
+        jobIndex = 1
+        easyApplyIndex = 1
+
+        jobLinkList = []
+        jobNameList = []
+        companyList = []
+
         for listing in soup.select('.result'): # Get job info from single url
-            jobName = listing.select_one('.jobTitle').get_text(' ')
+            job = listing.select_one('.jobTitle').get_text(' ') # Gets job title
+            jobName = job.split(' ', 1)[1] # Removes "new" from job title
             company = listing.select_one('.companyName').get_text(' ')
             try:
-                easyApply = listing.select_one('.ialbl.iaTextBlack').get_text(' ')
+                listing.select_one('.ialbl.iaTextBlack').get_text(' ') # Scrapes urls for "Easy Apply"
                 jobLink = f'https://indeed.com/viewjob?jk={listing["data-jk"]}'
-                print(f'#{easyApplyIndex} - Easily Apply - {jobLink} - {jobName} - {company}')
+                print(f'#{easyApplyIndex} - {jobLink} - {jobName} - {company}')
+                jobLinkList.append(jobLink)
+                jobNameList.append(jobName)
+                companyList.append(company)
                 easyApplyIndex +=1
+                data = {'Job Link': jobLinkList, 'Job Name': jobNameList, 'Company': companyList}
             except:
                 AttributeError
             jobIndex +=1
 
-def get_all_jobs_list():
-    print("get_all_jobs_list()")
-    jobIndex = 1
-    list = []
-    for urls in range(0,pageMax): # Get job info from multiple urls
-        for listing in soup.select('.result'): # Get job info from single url
-            jobName = listing.select_one('.jobTitle').get_text(' ')
-            company = listing.select_one('.companyName').get_text(' ')
-            location = listing.select_one('.companyLocation').get_text(' ')
-            jobLink = f'https://indeed.com/viewjob?jk={listing["data-jk"]}'
-            try:
-                easy_Apply = listing.select_one('.ialbl.iaTextBlack').get_text(' ')
-                print(f'#{jobIndex} - {jobLink} - **{easy_Apply}** - {jobName} - {company}')
-            except:
-                AttributeError
-                print(f'#{jobIndex} - {jobLink} - {jobName} - {company}')
-            jobIndex += 1
+    df = pd.DataFrame(data, columns= ['Job Link', 'Job Name', 'Company'])
+    df.to_csv('list.csv', index = False)
 
 
-def csvExport(): # Not working right!
-    df = pd.DataFrame(get_all_jobs_list(), columns=["job index", "joblink", "jobname", "company"])
-    df.to_csv('list.csv', index=False)
-
-get_all_jobs_list()
-get_all_jobs()
 get_easy_apply_links()
+
+
